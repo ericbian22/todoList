eeb31a07120d1578dac5ebb1bd94894ab4ce496d
Eric Bian <bianyaoda06@gmail.com>
2020-07-30 01:12:10 -0400

    v1 complete

---
+++ todolist-v1/date.js
@@ -0,0 +1,15 @@
+
+//or use module.exports.getData=getdate()
+// function getdate(){}
+
+exports.getDate= function (){
+let today=new Date();
+
+let options={
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
+ return today.toLocaleDateString("en-US",options);
+
+}
---
+++ todolist-v1/server.js
@@ -0,0 +1,52 @@
+var express=require("express");
+var bodyParser=require("body-parser");
+var app=express();
+var ejs=require("ejs");
+var date=require(__dirname+"/date.js");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+let items=["buy food","cook food","eat food"];
+let workItems=[];
+
+
+
+app.get("/",function(req,res){
+  let day=date.getDate();
+  res.render("list",{listTitle: day, newListItems: items});
+});
+
+app.post("/",function(req,res){
+  let item=req.body.newItem;
+  if(req.body.list==="Work"){
+      workItems.push(item);
+      res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+
+
+
+app.get("/work",function(req,res){
+   res.render("list",{listTitle: "Work List", newListItems: workItems})
+});
+
+app.post("/work",function(req,res){
+  let item=req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+
+app.get("/about",function(req,res){
+  res.render("about");
+});
+
+app.listen(2998,function(){
+  console.log("Server is running on port 2998");
+});
